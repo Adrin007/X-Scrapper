a029ecf75f4df50bab7300b318f36973b82e1357
Adrin007 <adrinteriffic44@gmail.com>
2024-12-29 00:07:39 +0530

    Final

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,7 @@ import chromedriver_binary
 from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 import os
@@ -15,7 +16,14 @@ def scrapper():
     X_USERNAME = os.getenv("X_USERNAME")
     X_PASSWORD = os.getenv("X_PASSWORD")
     proxy = ipPicker.pick()
-    driver = webdriver.Chrome()
+    chrome_driver_path = '/path/to/chromedriver'
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument("--no-sandbox") 
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    chrome_options.add_argument("--disable-gpu")
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     try:
         driver.get(website)
         usernameField = WebDriverWait(driver, 20).until(
