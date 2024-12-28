b7ee87539c5a6ce1c2072421dd223a55fb01fecc
Adrin007 <adrinteriffic44@gmail.com>
2024-12-29 00:18:16 +0530

    Holy Molly it 12AM and im still debugging since 9AM

--- scrapper.py
+++ scrapper.py
@@ -16,7 +16,7 @@ def scrapper():
     X_USERNAME = os.getenv("X_USERNAME")
     X_PASSWORD = os.getenv("X_PASSWORD")
     proxy = ipPicker.pick()
-    chrome_driver_path = '/usr/bin/chromedriver'
+    chrome_driver_path = '/path/to/chromedriver'
     chrome_options = Options()
     chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox") 
