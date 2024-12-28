a5cc758c48eb841a8482367f84d0283a9afa1ac9
Adrin007 <adrinteriffic44@gmail.com>
2024-12-28 23:38:59 +0530

    Docker Fix

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,7 @@ import chromedriver_binary
 from dotenv import load_dotenv
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import ipPicker
@@ -13,10 +14,12 @@ def scrapper():
     website = 'https://AdrinSanchez:Adrin_billnr01@x.com/i/flow/login'
     X_USERNAME = os.getenv("X_USERNAME")
     X_PASSWORD = os.getenv("X_PASSWORD")
-    # path = 'D:/Stir-Intern-Assignment/chromedriver-win64/chromedriver.exe'
-    # service = Service(path)
     proxy = ipPicker.pick()
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") 
+    chrome_options.add_argument("--no-sandbox")  
+    chrome_options.add_argument("--disable-dev-shm-usage") 
+    driver = webdriver.Chrome(options=chrome_options)
     try:
         driver.get(website)
         usernameField = WebDriverWait(driver, 20).until(
