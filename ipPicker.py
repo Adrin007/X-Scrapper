13d7ce6be1b974f877de0963123d18730517d165
Adrin007 <adrinteriffic44@gmail.com>
2024-12-28 22:36:47 +0530

    Final Fix

--- ipPicker.py
+++ ipPicker.py
@@ -2,7 +2,7 @@ import random
 availableProxy = {1:{"ip":"45.32.86.6","port":"31280"},2:{"ip":"45.32.231.36","port":"31280"},3:{"ip":"199.247.13.177","port":"31280"},4:{"ip":"136.68.111.53","port":"31280"},5:{"ip":"128.199.36.195","port":"31280"},6:{"ip":"45.63.75.212","port":"31280"}}
 def pick():
     randIndex = random.randint(1,6)
-    return (f"{availableProxy[randIndex]["ip"]}:{availableProxy[randIndex]["port"]}")
+    return (f'{availableProxy[randIndex]["ip"]}:{availableProxy[randIndex]["port"]}')
 
 def main():
     pick()
